c9c7e60561c9c3f5024aa2c686d0bf0b88ca9c90
sunlight0928 <info.tw91926@gmail.com>
2025-01-20 09:41:20 -0500

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -2,10 +2,14 @@ import { FC } from "react";
 
 export const Footer: FC = () => {
   return (
-    <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center justify-center">
-      <div className="font-bold text-2xl flex items-center hover:opacity-50">
-        AI Chatbot
-      </div>
-    </div>
+    <>
+      <p className="my-10 text-sm text-center text-gray-500">
+        &copy; {new Date().getFullYear()}{" "}
+        <a href="https://resume.karzus.com/dashboard/jobs" className="hover:underline">
+        RecruitGenie
+        </a>
+        . All rights reserved.
+      </p>
+    </>
   ); 
 };
