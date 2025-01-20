aa368434fa8ddcac55c741ba4335b89a0f982d0e
sunlight0928 <info.tw91926@gmail.com>
2025-01-20 09:41:44 -0500

    Update Navbar.tsx

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,16 +1,35 @@
 import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export const Navbar: FC = () => {
   return (
-    <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
-      <div className="font-bold text-3xl flex items-center">
-        <a
-          className="ml-2 hover:opacity-50"
-          href="https://resume.karzus.com/dashboard/jobs"
-        >
-          {"Go to RecruitGenie"}
-        </a>
+    <nav className="fixed z-30 w-full bg-white border-b border-gray-200">
+      <div className="px-3 py-3 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start">
+            {/* Logo */}
+            <Link href="https://resume.karzus.com/dashboard/jobs" className="flex ml-2 md:mr-3">
+              <Image
+                src="/media/svg/logo2.svg"
+                alt="Logo"
+                width={48}
+                height={40}
+                priority
+              />
+              <span className="ml-2 self-center text-xl font-semibold sm:text-2xl whitespace-nowrap">
+                {/* Dashboard */}
+              </span>
+            </Link>
+
+            <div className="font-bold text-xl p-1 bg-white shadow-sm" style={{ fontFamily: "sans-serif" }}>
+              <a href="https://resume.karzus.com/dashboard/jobs">
+                <h1>RecruitGenie</h1>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
