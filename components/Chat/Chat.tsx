c902ca2403b7ee0cc1037b6aac7475ecf45506b3
sunlight0928 <info.tw91926@gmail.com>
2025-01-20 09:40:56 -0500

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -15,7 +15,7 @@ interface Props {
 export const Chat: FC<Props> = ({ messages, loading, onSend, onReset }) => {
   return (
     <>
-      <div className="flex flex-row justify-between items-center mb-4 sm:mb-8">
+      <div className="flex flex-row justify-between items-center mb-4 sm:mb-8 mt-[100px]">
         <ResetChat onReset={onReset} />
       </div>
 
