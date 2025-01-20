66bf38870676bebf85d687a0c3766a1e5c2ec985
sunlight0928 <info.tw91926@gmail.com>
2025-01-20 08:00:03 -0500

    Update Navbar.tsx

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -6,9 +6,9 @@ export const Navbar: FC = () => {
       <div className="font-bold text-3xl flex items-center">
         <a
           className="ml-2 hover:opacity-50"
-          href="https://code-scaffold.vercel.app"
+          href="https://resume.karzus.com/dashboard/jobs"
         >
-          Chatbot UI
+          {"Go to RecruitGenie"}
         </a>
       </div>
     </div>
