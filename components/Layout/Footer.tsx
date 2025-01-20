7e2d38728ad0b8d6cd277706a69cfaba44798e14
sunlight0928 <info.tw91926@gmail.com>
2025-01-20 07:59:02 -0500

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react";
 
 export const Footer: FC = () => {
-  return <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center"></div>;
+  return (
+    <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center justify-center">
+      <div className="font-bold text-2xl flex items-center hover:opacity-50">
+        AI Chatbot
+      </div>
+    </div>
+  ); 
 };
